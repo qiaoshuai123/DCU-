6fcc1606a2a9788feb02578acd993307a44c087f
aleenXu <twtxogu@yeah.net>
2020-06-15 21:01:54 +0800

    update signalmanagement

---
+++ containers/SignalManagement/SignalManagement/InterworkingList/InterworkingList.jsx
@@ -0,0 +1,126 @@
+import React, { Component } from 'react'
+import { Input, Pagination, Select } from 'antd'
+import styles from './InterworkingList.scss'
+
+class InterworkingList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      systemList: [
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+        {
+          id: 3,
+        },
+        {
+          id: 4,
+        }
+      ],
+    }
+  }
+  componentDidMount = () => {
+
+  }
+  getresetPwd = (id) => {
+    window.open(`#roaddetail/${id}`)
+  }
+  handleChange = () => {
+
+  }
+  backPage = () => {
+    this.props.showInterworkingList(false)
+  }
+  render() {
+    const { systemList } = this.state
+    const { Option } = Select
+    return (
+      <div className={styles.syetem_bg} ref={(input) => { this.userLimitBox = input }}>
+        <div className={styles.syetem_title}>
+          <div className={styles.syetem_titleLeft}>DCU设备状态监视</div>
+          <div className={styles.syetem_titleRight} onClick={this.backPage} />
+        </div>
+        <div className={styles.syetem_top}>
+          <div className={styles.syetem_item}>
+            <span className={styles.item}>关键词:</span>
+            <div className={styles.inSle}><Input onChange={this.handleInputChange} /></div>
+          </div>
+          <div className={styles.syetem_item}>
+            <span className={styles.item}>行政区:</span>
+            <div className={styles.inSle}>
+              <Select defaultValue="行政一" style={{ width: 200, margin: 0 }} onChange={this.handleChange}>
+                <Option value="行政一">行政一</Option>
+                <Option value="行政二">行政二</Option>
+              </Select>
+            </div>
+          </div>
+          <div className={styles.syetem_item}>
+            <span className={styles.item}>点位名称:</span>
+            <div className={styles.inSle}>
+              <div className={styles.inSle}>
+                <Select defaultValue="点位一" style={{ width: 200, margin: 0 }} onChange={this.handleChange}>
+                  <Option value="点位一">点位一</Option>
+                  <Option value="点位二">点位二</Option>
+                </Select>
+              </div>
+            </div>
+          </div>
+          <span className={styles.searchBtn} onClick={() => { this.handlePagination('1') }} limitid="13">查询</span>
+        </div>
+        <div className={styles.equipmentList}>
+          设备列表
+        </div>
+        <div className={styles.syetem_buttom}>
+          <div className={styles.listBox}>
+            <div className={styles.listItems}>
+              <div className={styles.listTd} >点位名称</div>
+              <div className={styles.listTd} >设备编号</div>
+              <div className={styles.listTd} >设备型号</div>
+              <div className={styles.listTd} >设备IP</div>
+              <div className={styles.listTd} >生产厂商</div>
+              <div className={styles.listTd} >经纬度</div>
+              <div className={styles.listTd} >维护电话</div>
+              <div className={styles.listTd} >设备状态</div>
+              <div className={styles.listTd} >信号接入状态</div>
+              <div className={styles.listTd} >数据接入状态</div>
+              <div className={styles.listTd} >发布服务状态</div>
+              <div className={styles.listTd} >操作</div>
+            </div>
+            {systemList && systemList.map((item, index) => {
+              return (
+                <div className={styles.listItems} key={item.id + index}>
+                  <div className={styles.listTd} >11213213212222222222222222222222</div>
+                  <div className={styles.listTd} >2</div>
+                  <div className={styles.listTd} >3</div>
+                  <div className={styles.listTd} >4</div>
+                  <div className={styles.listTd} >5</div>
+                  <div className={styles.listTd} >6</div>
+                  <div className={styles.listTd} >7</div>
+                  <div className={styles.listTd} >8</div>
+                  <div className={styles.listTd} >9</div>
+                  <div className={styles.listTd} >10</div>
+                  <div className={styles.listTd} >11</div>
+                  <div className={styles.listTd} >
+                    <span className={styles.delectName} onClick={() => { this.getresetPwd(item.id) }}>
+                      路口监视
+                    </span>
+                  </div>
+                </div>)
+            })}
+            {
+              !!systemList && systemList.length === 0 ? <div className={styles.noData}>当前查询无数据</div> : null
+            }
+          </div>
+          <div className={styles.paginations}>
+            <Pagination showQuickJumper defaultCurrent={2} total={500} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default InterworkingList
--- containers/SignalManagement/SignalManagement/SignalManagement.jsx
+++ containers/SignalManagement/SignalManagement/SignalManagement.jsx
@@ -1,19 +1,139 @@
 import React, { Component } from 'react'
 import { Input } from 'antd'
 import Header from '../../../components/Header/Header'
+import markerIcon from '../../../images/markerGreen.png'
+import markerRedIcon from '../../../images/markerRed.png'
+import CustomTree from '../../../components/CustomTree/CustomTree'
+import InterworkingList from './InterworkingList/InterworkingList'
 import styles from './SignalManagement.scss'
 
+const pointArr = [
+  [120.113369,30.234277],
+  [120.421673,30.271644],
+  [120.251385,30.405574],
+  [120.208126,30.106052]
+]
 class SignalManagement extends Component {
   constructor(props) {
     super(props)
     this.state = {}
+    this.map = null
   }
+  componentDidMount = () => {
+    // 初始化地图
+    this.loadingMap()
+  }
+  // 创建地图层
+  loadingMap = () => {
+    const map = new AMap.Map('mapContent', {
+      resizeEnable: true, //是否监控地图容器尺寸变化
+      center: [120.202633, 30.266603], //初始化地图中心点
+      mapStyle: "amap://styles/f9281194c6119ea4669f1dd2e75af292",
+      zoom: 11,
+    })
+    this.map = map
+    map.on("click",function(e){
+      console.log(e.lnglat.getLng() + ',' + e.lnglat.getLat())
+    })
+    this.createLayerGroup('pointLayers') // map中显示点的图层
+    this.drawMarkers(pointArr, markerIcon, 'pointLayers') // 初始化点
+  }
+  // 创建地图层 > 对应元素层
+  createLayerGroup = (name) => {
+    window[name] = new AMap.LayerGroup({
+      'autoRefresh': true,     //是否自动刷新，默认为false
+      'interval': 180,         //刷新间隔，默认180s
+    });
+  }
+  //批量添加点
+  drawMarkers = (positions, imgIcon, layer) => {
+    const map = this.map
+    if (window[layer]) {
+      window[layer].removeLayers(this[layer])
+    }
+    this[layer] = []
+    if (this.infoWindow) {
+      this.infoWindow.close()
+    }
+    if (map) {
+      for (let i = 0; i < positions.length; i++) {
+        const latlng = positions[i]
+        // const latlng = positions[i].latlng
+        const marker = new AMap.Marker({
+          position: new AMap.LngLat(latlng[0], latlng[1]),
+          offset: new AMap.Pixel(-16, -16),
+          content: "<div class='marker-online'></div>",
+        })
+        marker.on('click', () => {
+          map.emit('click', {
+            lnglat : map.getCenter()
+          })
+          marker.setContent("<div class='drawCircle'><div class='inner'></div><div class='marker-online'></div></div>");
+          const nowZoom = map.getZoom()
+          map.setZoomAndCenter(nowZoom, positions[i]); //同时设置地图层级与中心点
+          this.openInfoWin(map, positions[i], marker)
+        })
+        this[layer].push(marker)
+      }
+      window[layer].addLayers(this[layer]) // 把点添加到层组中
+      window[layer].setMap(map) // 层组渲染到地图中
+    }
+  }
+  //在指定位置打开信息窗体
+  openInfoWin = (map, dataItem, marker) => {
+    var info = [];
+    // this.dataItem = JSON.parse(JSON.stringify(dataItem))
+    info.push(`<div class='content_box'>`);
+    info.push(`<div class='content_box_title'><h4>点位详情</h4>`);  
+    info.push(`<p class='input-item' style='border-top: 1px #838a9a solid;margin-top:-10px;padding-top:15px;'>点位名称：<span>` + '路口1' + `</span></p>`);    
+    info.push(`<p class='input-item'>信号机编号：<span>` + '1000010' + `</span></p>`);
+    info.push(`<p class='input-item'>信号机品牌：<span>` + '海信' + `</span></p>`);
+    info.push(`<p class='input-item'>设备IP：<span>` + '192.168.1.88' + `</span></p>`);
+    info.push(`<p class='input-item'>维护电话：<span>` + '01086861234' + `</span></p>`);
+    info.push(`<p class='input-item'>信号运行阶段：<span>` + '东西直行' +`<b class='icon_phase'></b></span></p>`);
+    info.push(`<p class='input-item'>信号运行方案：<span>` + '早高峰' + `</span></p>`);
+    info.push(`<p class='input-item'>信号控制方式：<span>` + '实时优化控制' + `</span></p>`);
+    info.push(`<p class='input-item' style='height:15px;'></p>`);  
+    info.push(`<p style='border-top: 1px #838a9a solid;margin-top:10px;' class='input-item'><span class="paramsBtn">参数配置</span></p>`);
+    const infoWindow = new AMap.InfoWindow({
+      content: info.join("")  //使用默认信息窗体框样式，显示信息内容
+    });
 
+    infoWindow.open(map, dataItem);
+    this.infoWindow = infoWindow
+    window.infoWindowClose = infoWindow
+    map.on('click', (e) => {
+      marker.setContent("<div class='marker-online'></div>");
+      infoWindow.close()
+    })
+  }
   render() {
     const { Search } = Input
     return (
       <div className={styles.SignalManagement}>
         <Header {...this.props} />
+        <div className={styles.Interwork_left}>
+          <div className={styles.InterworkLeft_search}>
+            <Search
+              placeholder="关键词搜索"
+              onSearch={value => console.log(value)}
+              style={{ width: 200 }}
+            />
+          </div>
+          <div className={styles.InterworkLeft_Title}>
+            <span />DCU点位列表
+          </div>
+          <CustomTree />
+        </div>
+        <div className={styles.mapContent} id='mapContent'>
+          <div className={styles.tagMarker}>
+            <div className={styles.statusBox}>
+              <span className={styles.tagOnLine}>在线设备9处</span>
+              <span className={styles.tagOffLine}>离线设备3处</span>
+            </div>
+            <div className={styles.turnBtn}></div>
+          </div>
+        </div>
       </div>
     )
   }
