72822bc89560018474c09cddb6ab22a329740e88
aleenXu <twtxogu@yeah.net>
2020-06-24 20:10:04 +0800

    update reducers

--- reactRedux/reducers/signalmanagement.js
+++ reactRedux/reducers/signalmanagement.js
@@ -13,100 +13,100 @@ const signalmanagement = (state = {}, action) => {
       return Object.assign({}, state, { basicUplSuccess: payload })
     // 车道配置
     case types.GET_LANE_INFO_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { laneLists: payload })
     case types.GET_LANE_PIC_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lanePicLists: payload })
     case types.POST_ADD_LANE_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { laneOtherData: payload })
     case types.POST_ADD_LANE_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { laneAddData: payload })
     case types.POST_UPDATE_LANE_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { laneUpdateOthers: payload })
     case types.POST_UPDATE_LANE_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { laneUpdatePic: payload })
     case types.GET_DEL_LANE_INFO:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { laneDelInfo: payload })
     case types.GET_DEL_LANE_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { laneDelPic: payload })
     // 灯组配置
     case types.GET_LIGHT_INFO_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lightLists: payload })
     case types.GET_LIGHT_PIC_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lightPicLists: payload })
     case types.POST_ADD_LIGHT_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lightOtherData: payload })
     case types.POST_ADD_LIGHT_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lightAddData: payload })
     case types.POST_UPDATE_LIGHT_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lightUpdateOthers: payload })
     case types.POST_UPDATE_LIGHT_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lightUpdatePic: payload })
     case types.GET_DEL_LIGHT_INFO:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lightDelInfo: payload })
     case types.GET_DEL_LIGHT_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { lightDelPic: payload })
     // 检测器配置
     case types.GET_DETECTOR_INFO_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { detectorLists: payload })
     case types.GET_DETECTOR_PIC_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { detectorPicLists: payload })
     case types.POST_ADD_DETECTOR_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { detectorOtherData: payload })
     case types.POST_ADD_DETECTOR_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { detectorAddData: payload })
     case types.POST_UPDATE_DETECTOR_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { detectorUpdateOthers: payload })
     case types.POST_UPDATE_DETECTOR_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { detectorUpdatePic: payload })
     case types.GET_DEL_DETECTOR_INFO:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { detectorDelInfo: payload })
     case types.GET_DEL_DETECTOR_PIC:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { detectorDelPic: payload })
     // 相位配置
     case types.GET_PHASE_INFO_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { phaseLists: payload })
     case types.POST_ADD_PHASE_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { phaseOtherData: payload })
     case types.POST_UPDATE_PHASE_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { phaseUpdateOthers: payload })
     case types.GET_DEL_PHASE_INFO:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { phaseDelInfo: payload })
     // 阶段配置
     case types.GET_STAGE_INFO_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { stageLists: payload })
     case types.POST_ADD_STAGE_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { stageOtherData: payload })
     case types.POST_UPDATE_STAGE_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { stageUpdateOthers: payload })
     case types.GET_DEL_STAGE_INFO:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { stageDelInfo: payload })
     // 配时方案配置
     case types.GET_PLAN_INFO_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { planLists: payload })
     case types.POST_ADD_PLAN_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { planOtherData: payload })
     case types.POST_UPDATE_PLAN_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { planUpdateOthers: payload })
     case types.GET_DEL_PLAN_INFO:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { planDelInfo: payload })
     // 日计划配置
     case types.GET_DAYPLAN_INFO_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { dayPlanLists: payload })
     case types.POST_ADD_DAYPLAN_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { dayPlanOtherData: payload })
     case types.POST_UPDATE_DAYPLAN_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { dayPlanUpdateOthers: payload })
     case types.GET_DEL_DAYPLAN_INFO:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { dayPlanDelInfo: payload })
     // 调度配置
     case types.GET_DISPATCH_INFO_LISTS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { dispatchLists: payload })
     case types.POST_ADD_DISPATCH_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { dispatchOtherData: payload })
     case types.POST_UPDATE_DISPATCH_OTHERS:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { dispatchUpdateOthers: payload })
     case types.GET_DEL_DISPATCH_INFO:
-      return Object.assign({}, state, { basicUplSuccess: payload })
+      return Object.assign({}, state, { dispatchDelInfo: payload })
     default:
       return state
   }
