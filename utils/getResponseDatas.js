d6f26dfbc3b574736c878c260c0ee794caceb4c7
zhangchaohui <15001300136@163.com>
2020-07-06 15:46:34 +0800

    add systemMange

--- utils/getResponseDatas.js
+++ utils/getResponseDatas.js
@@ -1,42 +1,49 @@
 import axios from 'axios'
+// import requestUrl from './getRequestBaseUrl'
 
-// const instance = axios.create({
-//   baseURL: 'http://192.168.1.213:20201'
-// })
-// // 请求拦截
-// instance.interceptors.request.use((config) => {
-//   const pathName = (config.url.split('/')).pop()
-//   if (pathName !== 'login') {
-//     const userInfo = JSON.parse(localStorage.getItem('userInfo'))
-//     if (userInfo) {
-//       config.headers.Authorization = userInfo.token
-//     } else {
-//       window.location.href = '#/login'
-//     }
-//   }
-//   return config
-// }, error => Promise.reject(error))
+const instance = axios.create({
+  // baseURL: requestUrl,
+})
+// 请求拦截
+instance.interceptors.request.use((config) => {
+  const pathName = (config.url.split('/')).pop()
+  if (pathName !== 'login') {
+    // 正式登录后开启
+    // const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+    // if (userInfo) {
+    //   config.headers.Authorization = userInfo.token
+    //   config.headers.singleToken = userInfo.singleToken
+    // } else {
+    //   window.location.href = '' // 登录失效后的跳转地址
+    // }
+    // 临时使用
+    // config.headers.Authorization = 'dcc6f4eff3b017a1b425051765f351b9d62bee5f9c0d7e9a3db056faedd3fe6fde7bf3a9d8d8e707edf4a1169f5eec1437c7eb859c135847c6db4983208aca47'
+    // config.headers.singleToken = '123456'
+  }
+  return config
+}, error => (Promise.reject(error)))
+
 // 返回拦截
-axios.interceptors.response.use((response) => {
+instance.interceptors.response.use((response) => {
   return response
 }, (error) => {
   return Promise.reject(error)
 })
-const getResponseDatas = async (type, url, requestParams) {
+const getResponseDatas = async (type, url, requestParams) => {
   try {
     let reponse = ''
     switch (type) {
       case 'get':
-        reponse = await axios.get(url, { params: requestParams })
+        reponse = await instance.get(url, { params: requestParams })
         break
       case 'post':
-        reponse = await axios.post(url, requestParams)
+        reponse = await instance.post(url, requestParams)
         break
       case 'put':
-        reponse = await axios.put(url, requestParams)
+        reponse = await instance.put(url, requestParams)
         break
       case 'delete':
-        reponse = await axios.delete(url, requestParams)
+        reponse = await instance.delete(url, requestParams)
         break
       default:
         break
