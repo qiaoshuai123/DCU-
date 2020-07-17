df0262dd7c24470761fede898afc1027efcd69d7
zhangchaohui <15001300136@163.com>
2020-07-17 11:45:35 +0800

    update

--- containers/SysteManagement/JurManagement/JurManagement.jsx
+++ containers/SysteManagement/JurManagement/JurManagement.jsx
@@ -102,7 +102,8 @@ class JurManagement extends Component {
     const { children, eventKey, dataRef } = e.node.props
     if (children.length > 0) {
       if (e.checked) {
-        this.defaultparams.menuIds += ',' + eventKey
+        const lengths = this.defaultparams.menuIds.length
+        this.defaultparams.menuIds += lengths > 0 ? ',' + eventKey: eventKey
         this.getChildKeys(children)
         const allCheckedKeys = Array.from(new Set(this.defaultparams.menuIds.split(',')))
         this.defaultparams.menuIds = allCheckedKeys.join(',')
