a69acf10c903132144f395b1e426b5fcb817a5bc
qiaoshuai <1522740101@qq.com>
2020-09-25 14:29:41 +0800

    'update'

--- utils/getRequestBaseUrl.js
+++ utils/getRequestBaseUrl.js
@@ -4,7 +4,7 @@ const getRequestBaseUrl = () => {
   if (process.env.NODE_ENV === 'development') {
     return 'http://192.168.1.213:20203'
   } else if (process.env.NODE_ENV === 'production') {
-    return 'http://33.89.129.41:20203'
+    return 'http://33.89.129.42:20203'
   }
 }
 export default getRequestBaseUrl
